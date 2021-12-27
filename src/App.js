188f3e41e132c1f2cf1eceb349c3fd820fceade1
MuhammadHafijur <hafijur.salah@gmail.com>
2021-12-27 23:29:33 +0600

    added dynamic route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./Pages/Home/Home/Home";
+import { Route, Routes } from "react-router-dom";
+import ShowDetails from "./Pages/ShowDetails/ShowDetails";
 
 function App() {
   return (
     <div className="">
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/showDetails/:showId" element={<ShowDetails />} />
+      </Routes>
     </div>
   );
 }
--- src/Pages/Home/Show/Show.js
+++ src/Pages/Home/Show/Show.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Show = (props) => {
-    const {name, image} = props.show;
+    const {name, id, image} = props.show;
 
     return (
         <div className="">
             <div className="h-4/5 w-60 mx-auto">
-            <img className="w-full h-full" src={image?.original} alt="" />
+            {/* <img className="w-full h-full" src={image?.original} alt="" /> */}
             <h1>{name}</h1>
-            <button className="px-8 py-2 text-white font-semibold rounded text-base bg-blue-500 shadow-lg shadow-blue-500/50 ...">See Details</button>
+            <Link to={`/showDetails/${id}`} className="px-8 py-2 text-white font-semibold rounded text-base bg-blue-500 shadow-lg shadow-blue-500/50 ...">See Details</Link>
             </div>
         </div>
     );
