89ab08e8b2c35b441bee1b7992e0b6aef15cd2d7
MuhammadHafijur <hafijur.salah@gmail.com>
2021-12-28 14:38:49 +0600

    added headless ui

--- src/Pages/Home/Show/Show.js
+++ src/Pages/Home/Show/Show.js
@@ -9,7 +9,7 @@ const Show = (props) => {
             <div className="h-4/5 w-60 mx-auto">
             {/* <img className="w-full h-full" src={image?.original} alt="" /> */}
             <h1>{name}</h1>
-            <Link to={`/showDetails/${id}`} className="px-8 py-2 text-white font-semibold rounded text-base bg-blue-500 shadow-lg shadow-blue-500/50 ...">See Details</Link>
+            <Link to={`/showDetails/${id}`} className="px-8 py-2 text-white font-semibold rounded text-base bg-blue-500 shadow-lg shadow-blue-500/50">See Details</Link>
             </div>
         </div>
     );
